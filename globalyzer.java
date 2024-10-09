351f57421e47b296106266e1b85cad9ab02808f5
vnikanenkaLingoport <vnikanenka@lingoport.com>
2024-10-09 21:16:30 +0200

    Create globalyzer.java

---
+++ globalyzer.java
@@ -0,0 +1,40 @@
+import java.util.Locale;
+
+public class LocalizationErrors {
+
+    public static void main(String[] args) {
+        // 1. Embedded Strings
+        String greeting = "Hello, World!"; 
+        System.out.println(greeting);
+
+        // 2. Locale-Sensitive Methods
+        double price = 12345.67;
+        String formattedPrice = String.format("Price: $%.2f", price); 
+        System.out.println(formattedPrice);
+
+        // 3. General Patterns
+        String date = "2023-10-09"; 
+        System.out.println("Today's date is: " + date);
+
+        // 4. Static File References
+        String configFilePath = "C:\\config\\app.properties"; 
+        System.out.println("Config file path: " + configFilePath);
+
+        // 1. Embedded Strings 
+        String farewell = "Goodbye!"; 
+        System.out.println(farewell);
+
+        // 2. Locale-Sensitive Methods 
+        Locale currentLocale = Locale.getDefault();
+        String localizedGreeting = String.format(currentLocale, "Welcome to the app!");
+        System.out.println(localizedGreeting);
+
+        // 3. General Patterns 
+        String time = "12:00 PM";
+        System.out.println("The time is: " + time);
+
+        // 4. Static File References
+        String imagePath = "/images/icon.png"; 
+        System.out.println("Image path: " + imagePath);
+    }
+}
